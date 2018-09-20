5aa3d1843d88af83c9449fc7a227cf69ade83c8a
Michael McDermott <mmcdermott18@gmail.com>
2018-09-20 10:49:39 -0400

    program

--- js/main.js
+++ js/main.js
@@ -7,22 +7,27 @@ $(document).ready(function() {
 
     // run function on resize of the window
     $(window).resize(function() {
-      studentHeight();
-    });
+      
+    })
     // run function on scroll
     $(window).scroll(function() {
       
-    });
+    })
 });
 
 var textHeight = $('.student h2').outerHeight();
 
 function studentHeight() {
   var headHeight = $('header').outerHeight();
+  console.log('Header Height ' + headHeight);
   var pageHeight = $(window).height();
+  console.log('Page Height ' + pageHeight);
   var height = ((pageHeight - headHeight) / 4);
+  console.log('Height ' + height);
   var topPadding = ((height - textHeight) / 2);
+  console.log('Top Padding ' + topPadding);
   $('.student').css("height", height);
+  $('.student').css("background-color", height);
   $('.menu-list').css("height", (height * 4));
   $('.student h2').css("padding-top", topPadding);
 }
